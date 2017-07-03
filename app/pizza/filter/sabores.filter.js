b1958c52a1d2e2672fe26a65fd4645955754f83f
Herbertt Teixeira <herbertt923@gmail.com>
2017-07-03 13:43:49 -0300

    [New] Arquivo que ordena os sabores das pizzas.
    Esse arquivo ordena os sabores da pizza.

---
+++ app/pizza/filter/sabores.filter.js
@@ -0,0 +1,19 @@
+(function() {
+    'use strict';
+
+    angular.module('app').filter('orderObjectBy', orderObjectBy);
+
+    function orderObjectBy(){
+        return function(items, field, reverse) {
+            var filtered = [];
+            angular.forEach(items, function(item) {
+                filtered.push(item);
+            });
+            filtered.sort(function (a, b) {
+                return (a[field] > b[field] ? 1 : -1);
+            });
+            if(reverse) filtered.reverse();
+            return filtered;
+        };
+    }
+ })();
