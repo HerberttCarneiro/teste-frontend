d6ec349abefc8c9ca871d86e9a38d24ff8461fa7
Herbertt Teixeira <herbertt923@gmail.com>
2017-07-03 13:08:26 -0300

    [Mod] Alterações no karma.config
    Acrescentando os arquivos necessarios para o teste.
    
    Novos plugins
    karma-phantomjs, karma-coverage.
    
    Mudança no browsers de Chrome para PhantomJS.

--- karma.conf.js
+++ karma.conf.js
@@ -5,25 +5,50 @@ module.exports = function(config) {
     basePath: './app',
 
     files: [
-      'bower_components/angular/angular.js',
-      'bower_components/angular-route/angular-route.js',
-      'bower_components/angular-mocks/angular-mocks.js',
+      '../bower_components/angular/angular.js',
+      '../bower_components/angular-route/angular-route.js',
+      '../bower_components/angular-mocks/angular-mocks.js',
       'components/**/*.js',
-      'view*/**/*.js'
+      'app.module.js',
+      'app.config.js',
+      'pizza/pizza.module.js',
+      'pizza/pizza.constant.js',
+      'pizza/pizza.service.js',
+      'pizza/controller/menu.controller.js',
+      'pizza/controller/menu.controller.spec.js',
+      '**/*.spec.js',
+      // '**/*.module.js',
+      // '**/*.js'
     ],
 
     autoWatch: true,
 
     frameworks: ['jasmine'],
 
-    browsers: ['Chrome'],
+    browsers: ['PhantomJS'],
+
+    reporters: ['progress', 'coverage'],
 
     plugins: [
-      'karma-chrome-launcher',
+      'karma-phantomjs-launcher',
       'karma-firefox-launcher',
       'karma-jasmine',
-      'karma-junit-reporter'
+      'karma-junit-reporter',
+      'karma-coverage'
     ],
+
+    preprocessors: {
+      // source files, that you wanna generate coverage for
+      // do not include tests or libraries
+      // (these files will be instrumented by Istanbul)
+      '/**/*.js': ['coverage']
+    },
+
+    // optionally, configure the reporter
+    coverageReporter: {
+      type : 'html',
+      dir : '../coverage/'
+    },
 
     junitReporter: {
       outputFile: 'test_out/unit.xml',
